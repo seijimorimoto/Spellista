6fa07c5dd55184c0066bdf244d0f59494c0a8abc
Jonathan Cárdenas <JonathanCrd@users.noreply.github.com>
2019-08-24 18:24:24 -0500

    Align items to center

--- UI/views/launch_screen/launch_screen.tsx
+++ UI/views/launch_screen/launch_screen.tsx
@@ -4,7 +4,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
   return (
-    <LinearGradient colors={['#e94057', '#ff1493', '#b000af', '#3f0081']} style={{flex: 1, width: '100%'}}>
+    <LinearGradient colors={['#e94057', '#ff1493', '#b000af', '#3f0081']} style={{flex: 1, width: '100%', alignItems: 'center', justifyContent: 'center'}}>
       <Button
         onPress={() => console.log('Login')}
         title="Log-in using Spotify"
