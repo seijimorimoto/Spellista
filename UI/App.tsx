da6a4078e7490ac14c3be85b8903c9bc0117efb4
renata-sg <renata.saldivar97@gmail.com>
2019-08-24 18:01:13 -0500

    Add Launch screen view

--- UI/App.tsx
+++ UI/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import LaunchScreen from './views/launch_screen/launch_screen';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <LaunchScreen></LaunchScreen>
     </View>
   );
 }
