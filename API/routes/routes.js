76b7ea6afa3eb058750fc56379cc2acb693a1229
Angel Seiji Morimoto Burgos <angel_seiji@hotmail.com>
2019-08-25 01:10:27 -0500

    Added first sample endpoint for retrieving playlists' names

--- API/routes/routes.js
+++ API/routes/routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const spellistaCtrl = require('../controllers/spellistaController');
 const router = express.Router();
 
-router.route('/playlist').post(spellistaCtrl.getSpellistas);
+router.route('/playlists').get(spellistaCtrl.getPlaylists);
 
 module.exports = router;
--- API/util/constants.js
+++ API/util/constants.js
@@ -1,3 +1,4 @@
 module.exports = {
-  DB_CONNECTION_STRING: process.env.CLEARDB_DATABASE.URL
+  DB_CONNECTION_STRING: process.env.CLEARDB_DATABASE_URL || 'mylocalconnection',
+  SPOTIFY_BASE_URI: 'https://api.spotify.com/v1/'
 };
