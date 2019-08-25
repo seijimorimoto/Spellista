4d5c6ca477e97734c4a6554c17820b60eb62a5f1
Angel Seiji Morimoto Burgos <angel_seiji@hotmail.com>
2019-08-25 01:10:27 -0500

    Added first sample endpoint for retrieving playlists' names

--- API/controllers/spellistaController.js
+++ API/controllers/spellistaController.js
@@ -1,3 +1,19 @@
+const Axios = require('axios');
+const constants = require('../util/constants');
+
 module.exports = {
-  getSpellistas: (req, res) => { }
+  getPlaylists: (req, res) => {
+    const { SPOTIFY_BASE_URI } = constants;
+    const { authorization } = req.headers;
+    const config = {
+      headers: {
+        'Authorization': authorization
+      }
+    };
+
+    Axios.get(SPOTIFY_BASE_URI + 'me/playlists', config).then(response => {
+      const playlists = response.data.items.map(playlist => playlist.name);
+      res.send(JSON.stringify(playlists));
+    });
+  }
 }
