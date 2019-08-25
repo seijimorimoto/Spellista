78ec98d39d3b9a51df1241d439964890223be73b
renata-sg <renata.saldivar97@gmail.com>
2019-08-24 19:48:17 -0500

    Add playlist screen view

---
+++ UI/views/playlist_screen/playlist_screen.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+
+export default function PlaylistScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.header}>Lover</Text>
+      <FlatList style={styles.list}
+        data={[
+          {key: 'I Forgot That You Existed'},
+          {key: 'Cruel Summer'},
+          {key: 'Lover'},
+          {key: 'The Man'},
+          {key: 'The Archer'},
+          {key: 'I Think He Knows'},
+          {key: 'Miss Americana & The Heartbreak Prince'},
+          {key: 'Paper Rings'},
+          {key: 'Cornelia Street'}
+        ]}
+        renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+   flex: 1,
+   paddingTop: 22
+  },
+  list: {
+    paddingTop: 20
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+  },
+  header: {
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 40
+  }
+})
